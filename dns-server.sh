67851d332112f4db14c15e6ac26b985f42f2d443
Sofyan Rahmat Hidaya <163423638+iansofyan@users.noreply.github.com>
2025-02-16 16:16:30 +0700

    Update dns-server.sh

--- dns-server.sh
+++ dns-server.sh
@@ -20,24 +20,37 @@ echo "nama domain kamu"
 read domain
 echo "ip anda"
 read ip
+echo "domain proxmox"
+read domainpx
+echo "ip proxmox"
+read ippx
 
 cp -R /etc/bind/db.local /etc/bind/${domain}
 cp -R /etc/bind/db.127 /etc/bind/${domain}.reverse
 
 #edit file
+sed -n '13p' /etc/bind/${domain} >> /etc/bind/${domain}
 sed -i "s/localhost/${domain}/g" /etc/bind/${domain}
 sed -i "s/::1/${domain}./g" /etc/bind/${domain}
 sed -i "14s/@/www.${domain}./g" /etc/bind/${domain}
-sed -i "s/127.0.0.1/${ip}/g" /etc/bind/${domain}
+sed -i "13s/127.0.0.1/${ip}/g" /etc/bind/${domain}
 sed -i "s/AAAA/CNAME/g" /etc/bind/${domain}
+sed -i "15s/@/${domainpx}/g" /etc/bind/${domain}
+sed -i "15s/127.0.0.1/${ippx}/g" /etc/bind/${domain}
 
 echo "Ip belakang anda"
 read ip2
 
+echo "Ip proxmox belakang anda"
+read ip3
+
+sed -n '13p' /etc/bind/${domain}.reverse >> /etc/bind/${domain}.reverse
 sed -n '13p' /etc/bind/${domain}.reverse >> /etc/bind/${domain}.reverse
 sed -i "s/localhost/${domain}/g" /etc/bind/${domain}.reverse
-sed -i "s/1.0.0/${ip2}/g" /etc/bind/${domain}.reverse
+sed -i "s14/1.0.0/${ip2}/g" /etc/bind/${domain}.reverse
+sed -i "s15/1.0.0/${ip3}/g" /etc/bind/${domain}.reverse
 sed -i "14s/${domain}/www.${domain}/g" /etc/bind/${domain}.reverse
+sed -i "15s/${domain}/${domainpx}/g" /etc/bind/${domain}.reverse
 
 #edit file named.local
 tee /etc/bind/named.conf.local << EOF > /dev/null
@@ -50,6 +63,10 @@ zone "${domain}" {
 zone "10.10.10.in-addr.arpa" {
         type master;
         file "/etc/bind/${domain}.reverse";
+};
+zone "${domainpx}" {
+        type master;
+        file "/etc/bind/${domain}";
 };
 EOF
 
